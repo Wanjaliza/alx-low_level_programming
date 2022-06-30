c2edc8bd9e68b40fc8c8d44fa6b85fcd89fbf1e6
Ian Kisali <iankisali@gmail.com>
2022-06-30 06:51:08 -0700

    Making corrections

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,4 @@
 #include<stdio.h>
-/** 
- * This is a code that prints output
- */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,4 @@
 #include<stdio.h>
-/**
- *  program that prints exactly with proper grammar, but the outcome is a piece of art,, followed by a new line
- */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
