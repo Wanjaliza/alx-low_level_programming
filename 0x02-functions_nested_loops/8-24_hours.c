aea98776baef1522602d2a4b8fbaa892502d3520
Ian Kisali <iankisali@gmail.com>
2022-07-05 09:19:44 -0700

    Submitting mandatory tasks

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+* jack_bauer - prints every minute of the day
+* Return: void
+*/
+void jack_bauer(void)
+{
+int num1, num2, num3, num4;
+for (num1 = 0; num1 <= 2; num1++)
+	{
+	for (num2 = 0; num2 <= 3; num2++)
+		{
+		for (num3 = 0; num3 <= 5; num3++)
+			{
+			for (num4 = 0; num4 <= 9; num4++)
+				{
+				_putchar('0' + num1);
+				_putchar('0' + num2);
+				_putchar(':');
+				_putchar('0' + num3);
+				_putchar('0' + num4);
+				_putchar('\n');
+				}
+			}
+		}
+	}
+}
