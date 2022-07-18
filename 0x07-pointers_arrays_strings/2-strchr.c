890d4d50baaf72fb3709d31701731550ebd7d9ba
Ian Kisali <iankisali@gmail.com>
2022-07-18 03:21:33 -0700

    added task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,9 +10,9 @@ char *_strchr(char *s, char c)
 	unsigned int num;
 
 	for (num = 0; *(s + num) != '\0'; num++)
-	{
 		if (*(s + num) == c)
 			return (s + num);
-	}
+	if (*(s + num) == c)
+		return (s + num);
 	return ('\0');
 }
