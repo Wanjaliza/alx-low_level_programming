93610fbe7314c3a04d4487e492ed87029a266395
Ian Kisali <iankisali@gmail.com>
2022-08-01 05:13:49 -0700

    added task 2

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 /**
-* prints the name of the file it was compiled from, followed by a new line.
+* main - prints the name of the file it was compiled from, followed by a new line.
+* Return: always 0
 */
 int main(void)
 {
