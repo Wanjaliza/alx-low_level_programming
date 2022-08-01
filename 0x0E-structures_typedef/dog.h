9b5638ff9cfc9dc2d60593b6c165b5b52943f328
Ian Kisali <iankisali@gmail.com>
2022-08-01 09:31:55 -0700

    made changes to header file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,4 +16,7 @@ typedef struct dog
 #ifndef _FUNCTIONS_H
 #define _FUNCTIONS_H
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif
