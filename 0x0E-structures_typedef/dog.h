bd2fcb4ef246cd2aeda278989b3df51b4ae569a9
Ian Kisali <iankisali@gmail.com>
2022-08-01 08:52:50 -0700

    added task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef STRUCTS
+#define STRUCTS
+/**
+* struct dog - description of pet
+* @name: pet name
+* @age: pet age
+* @owner: pet owner
+*/
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}dog_t;
+#endif
